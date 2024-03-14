eca8b3603648d6ef2ce3968c9e1569385af9fdfc
Lydia <lida.presnukhina@mail.ru>
2024-03-14 19:58:04 +0300

    я добавила чтение файла, но пока не читается

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ from kivy.uix.layout import Layout
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.screenmanager import SlideTransition
 from kivy.uix.widget import Widget
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ObjectProperty
+
 Builder.load_string("""
 <StartScreen>:
     FloatLayout:
         orientation: 'vertical'
-        padding:20
+        padding:40
         canvas:
             Color:
                 rgba: 1, .3, .8, .5
@@ -29,48 +30,81 @@ Builder.load_string("""
             pos_hint:{'center_x': .5, 'center_y': .3}
             on_press: root.manager.current = 'menu'
 <MenuScreen>
-    BoxLayout:
+    FloatLayout:
+        orientation: 'vertical'
+        padding:20
+        canvas:
+            Color:
+                rgba: 1, .3, .8, .5
         Label:
             text: 'You can choose your vibe'
+            pos_hint:{'center_x': .5, 'center_y': .7}
         Button:
             text: 'first'
+            size_hint: 0.2, 0.2
+            pos_hint:{'center_x': .3, 'center_y': .3}
             on_press: 
                 root.manager.current = 'first'
                 root.manager.transition.direction = 'left'
             
         Button:
             text: 'second'
+            size_hint: 0.2, 0.2
+            pos_hint:{'center_x': .7, 'center_y': .3}
             on_press: 
                 root.manager.current = 'second'
                 root.manager.transition.direction = 'left'
 <FirstScreen>
-    BoxLayout:
-        size:root.size
+    FloatLayout:
+        orientation: 'vertical'
+        padding:20
+        canvas:
+            Color:
+                rgba: 1, .3, .8, .5
         Label:
-            id:label1
-            text: 'lddldlld'
+            id:name_label
+            text: 'question is'
+    
         Button:
             id: button_question
             text: 'next question'
-            
+            size_hint: 0.2, 0.2
+            pos_hint:{'center_x': .3, 'center_y': .3}
         Button:
             text: 'change vibe'
+            size_hint: 0.2, 0.2
+            pos_hint:{'center_x': .7, 'center_y': .3}
             on_press: 
                 root.manager.current = 'menu'
                 root.manager.transition.direction = 'right'
 
 <SecondScreen>
-    BoxLayout:
+    FloatLayout:
+        orientation: 'vertical'
+        padding:20
+        canvas:
+            Color:
+                rgba: 1, 0, 1, 1
         Label:
             text: 'Второй вайб'
-            
+        Button:
+            id: button_question
+            text: 'next question'
+            size_hint: 0.2, 0.2
+            pos_hint:{'center_x': .3, 'center_y': .3}    
         Button:
             text: 'change vibe'
+            size_hint: 0.2, 0.2
+            pos_hint:{'center_x': .7, 'center_y': .3}
             on_press: 
                 root.manager.current = 'menu'
                 root.manager.transition.direction = 'right'
+
+        
 """)
 class MainApp(App):
+
+
     def build(self):
         # crate the screen manager
         sm =ScreenManager()
@@ -80,12 +114,28 @@ class MainApp(App):
         sm.add_widget(SecondScreen(name='second'))
         return sm
 
+
 class StartScreen(Screen):
     pass
 class MenuScreen(Screen):
     pass
 class FirstScreen(Screen):
-    pass
+
+    def question_label_text(self):
+        with open("text.txt") as f:
+           question = f.read()
+        # main_label.text = question
+        return question
+    def new_question(self):
+        # Create variables for our widget
+       # name = self.ids.name_input.text
+
+        # Update the label
+        self.ids.name_label.text = self.question_label_text()
+
+        # Clear input box
+        #self.ids.name_input.text = ''
+
 class SecondScreen(Screen):
     pass
 
