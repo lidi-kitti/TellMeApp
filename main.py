6ab67d09868e974cc81dc6cc1ebad7b24bd1e581
Лидия Преснухина <lida.presnukhina@mail.ru>
2024-04-18 15:21:45 +0300

    чуть-чуть испрвила текст и добавила новый файл с вопросами для второй кнопки

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 import random
-
-import kivy.uix.label
-from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.layout import Layout
 from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.uix.screenmanager import SlideTransition
-from kivy.uix.widget import Widget
-
 from kivy.properties import StringProperty, ObjectProperty
-Builder.load_file('design.kv')
+Builder.load_file('design.kv.txt')
 class MainApp(App):
-
-
     def build(self):
         # crate the screen manager
         sm =ScreenManager()
@@ -26,8 +13,6 @@ class MainApp(App):
         sm.add_widget(FirstScreen(name='first'))
         sm.add_widget(SecondScreen(name='second'))
         return sm
-
-
 class StartScreen(Screen):
     pass
 class MenuScreen(Screen):
@@ -43,13 +28,12 @@ class FirstScreen(Screen):
         return self.name_label_text
     def change_text(self):
 
-        a = str(open("text.txt","r",encoding="utf8").read()) #читаем файл
+        a = str(open("text_first_vibe.txt", encoding="utf8").read()) #читаем файл
         #a = a.split('\n')
         a = a.split(';\n')
         print(a)
         randomOffset = random.randrange(0, len(a))
         self.lbl.text = a[randomOffset] #рандомно строку выводим
-
 class SecondScreen(Screen):
     name_label_text = StringProperty()
 
@@ -60,12 +44,11 @@ class SecondScreen(Screen):
         return self.name_label_text
 
     def change_text(self):
-        a = str(open("text.txt", "r", encoding="utf8").read())  # читаем файл
-        a = a.split('\n')
+        a = str(open("text_second_vibe.txt", encoding="utf8").read())  # читаем файл
+        a = a.split(';\n')
         print(a)
         randomOffset = random.randrange(0, len(a))
         self.lbl.text = a[randomOffset]  # рандомно строку выводим
-
 if __name__ == '__main__':
     app = MainApp()
     app.run()
